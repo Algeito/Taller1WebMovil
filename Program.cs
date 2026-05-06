a8e7138bf72470c50256b0c29d12eb285a068df9
Juampa <136938655+Algeito@users.noreply.github.com>
2026-05-05 23:06:06 -0400

    feat(serilog): add and configure serilog

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region Logging Configuration
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
+#endregion
 
 builder.Services.AddOpenApi();
 
